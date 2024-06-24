aaa38554e29021c20d070bdd1ae0aae113c49a81
Ajgar <mdajgarhossain1@gmail.com>
2024-06-24 12:41:57 +0600

    Resolved product search input issue

--- app/page.js
+++ app/page.js
@@ -69,6 +69,21 @@ export default function Products() {
     <>
       <Navbar cartItems={cart} />
       <div className="bg-gray-200 pt-6 pb-10">
+        <div className="flex justify-end px-4 md:px-6 lg:px-10 pb-6">
+          <form
+            onSubmit={handleSearch}
+            className="relative w-full max-w-sm"
+          >
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search products..."
+              className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <FaSearch className="absolute left-4 top-4 text-gray-400" />
+          </form>
+        </div>
         {isLoading ? (
           <div className="flex justify-center items-center h-screen">
             <FaSpinner className="text-6xl animate-spin text-gray-400" />
@@ -78,22 +93,7 @@ export default function Products() {
             <p>{error}</p>
           </div>
         ) : (
-          <div className="">
-            <div className="flex justify-end px-4 md:px-6 lg:px-10 pb-6">
-              <form
-                onSubmit={handleSearch}
-                className="relative w-full max-w-sm"
-              >
-                <input
-                  type="text"
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  placeholder="Search products..."
-                  className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
-                <FaSearch className="absolute left-4 top-4 text-gray-400" />
-              </form>
-            </div>
+          <>
             <div className="grid px-4 md:px-6 lg:px-10 pb-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
               {products.map((product) => (
                 <ProductCard
@@ -115,7 +115,7 @@ export default function Products() {
                 <p>No products available.</p>
               </div>
             )}
-          </div>
+          </>
         )}
       </div>
     </>
